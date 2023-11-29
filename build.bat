63b2133d6cf4c57279ec019b29eb5a966a356c2d
lbbasilio <lbasilio@tutanota.com>
2023-11-29 03:49:53 -0300

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-rm parser.exe
-gcc -g -o parser.exe http_parser.c
+rm main.exe
+gcc -g -c -o bin\http_parser.o http_parser.c
+gcc -g -c -o bin\main.o main.c
+gcc -g -o main.exe bin\http_parser.o bin\main.o
